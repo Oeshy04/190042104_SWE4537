b5c75fe8321b1ab082e907d59956ac7e39700c1c
nafisatabassum4@iut-dhaka.edu <nafisatabassum4@iut-dhaka.edu>
2022-10-23 09:18:31 +0600

    completed tasks of lab 04

---
+++ ExpressWithMongoDB/routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const homeController = require("./controllers/homeController");
+const bookController = require("./controllers/bookController");
+
+router.get("/", homeController.getHome);
+router.get("/book-list", bookController.getBookList);
+router.get("/books", bookController.getBook);
+router.post("/books", bookController.postBook);
+router.get('/book-list/delete/:id', bookController.deleteBook);
+router.get("/updateBook/:id", bookController.getUpdateBook);
+router.post("/updateBook/:id", bookController.updateBook);
+
+module.exports = router;
